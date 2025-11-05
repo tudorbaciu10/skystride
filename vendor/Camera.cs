99eee04018bf1f8009ecfecbb1624b4341976687
Cătălin Grigoriev <catalingrigoriev50@gmail.com>
2025-11-05 14:46:55 +0200

    Remove project files and add Camera class stub
    Deleted solution and configuration files, including App.config, AssemblyInfo.cs, and skystride.sln, likely in preparation for restructuring the project. Added a stub for the Camera class in vendor/Camera.cs. Updated .gitignore to include 'debug' and reordered entries.

---
+++ vendor/Camera.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace skystride.vendor
+{
+    internal class Camera
+    {
+
+    }
+}
