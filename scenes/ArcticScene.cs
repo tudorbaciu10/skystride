c41306abdd95ecb66d9c4da9c0e9ec71fe3bbd5b
Cătălin Grigoriev <catalingrigoriev50@gmail.com>
2025-11-08 12:42:14 +0200

    Add second spawn plane to ArcticScene
    Introduces a new plane entity at position (0, 0, 20) with the same texture and properties as the original spawn plane, enhancing the scene setup.

--- scenes/ArcticScene.cs
+++ scenes/ArcticScene.cs
@@ -19,6 +19,10 @@ namespace skystride.scenes
             Plane spawnPlane = new Plane(new Vector3(0f, 0f, 0f), 20f, 10f, 1f, Color.IndianRed, new Vector3(0f, 1f, 0f));
             spawnPlane.SetTexture("assets/textures/survivor.png");
             AddEntity(spawnPlane);
+
+            Plane spawnPlane2 = new Plane(new Vector3(0f, 0f, 20f), 20f, 10f, 1f, Color.IndianRed, new Vector3(0f, 1f, 0f));
+            spawnPlane2.SetTexture("assets/textures/survivor.png");
+            AddEntity(spawnPlane2);
         }
     }
 }
